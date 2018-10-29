15e3880e8f2cef1735c1dc8e7a0daa11b6493dac
Fer Hernandez <fh.1111@gmail.com>
2018-10-29 01:26:37 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,38 @@
 
+
+def sort_array_asc (numbers)
+  array.sort do |a, b|
+    a <=> b
+  end
+end
+
+
+def sort_array_desc (numbers)
+  
+end
+
+
+def sort_array_char_count (numbers)
+  
+end
+
+
+def swap_elements (numbers)
+  
+end
+
+def reverse_array (numbers)
+  
+end
+
+def kesha_maker (numbers)
+
+end
+
+def find_a (numbers)
+
+end
+
+def add_s(numbers)
+
+end
